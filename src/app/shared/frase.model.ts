2b008dae746551da284e4923db4e4a70dfa58472
Pedro <ph_tavares@hotmail.com>
2022-08-09 11:45:38 -0300

    Criação do modelo e mock de frases

---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,9 @@
+export class Frase {
+    fraseEng: string;
+    frasePtBr: string;
+
+    constructor(fraseEng: string, frasePtBr: string) {
+        this.fraseEng = fraseEng;
+        this.frasePtBr = frasePtBr;
+    }
+}
