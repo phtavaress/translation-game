29126b185520239b71a34e2f778c92f268e620cb
Pedro <ph_tavares@hotmail.com>
2022-08-10 10:34:12 -0300

    Componente progresso

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,13 +11,33 @@ export class PainelComponent implements OnInit {
 
   frases: Frase[] = FRASES;
   instrucao: string = 'Traduza a frase';
+  resposta: string;
+  rodada: number = 0;
+  rodadaFrase: Frase;
+  progresso: number = 0;
 
-  constructor() { console.log(this.frases) }
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase);
+  }
 
   ngOnInit(): void {
   }
 
-  atualizaResposta(): void {
-    console.log('teste');
+  atualizaResposta(resposta: Event): void {
+    this.resposta = ((<HTMLInputElement>resposta.target).value);
+    //console.log(this.resposta);
+  }
+
+  verificarResposta(): void {
+    if(this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A tradução está correta!');
+      this.rodada++;
+      this.rodadaFrase = this.frases[this.rodada];
+      this.progresso = this.progresso + (100 / this.frases.length);
+      console.log(this.progresso);
+    } else {
+      alert('A tradução está errada.');
+    }
   }
 }
